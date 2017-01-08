455e4a26decd1530c866454b39b8c705cbe8ca49
Bianca Denise <biancadenise.delpuerto@bicol-u.edu.ph>
2017-01-08 21:53:36 +0800

    Initial Commit

---
+++ app/src/main/java/com/biancadenise/javamailapi2/Config.java
@@ -0,0 +1,10 @@
+package com.biancadenise.javamailapi2;
+
+/**
+ * Created by Bianca Denise on 08/01/2017.
+ */
+
+public class Config {
+    public static final String EMAIL ="biancadenisedelpuerto12@gmail.com";
+    public static final String PASSWORD ="PeanutButter12_google";
+}
